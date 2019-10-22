a78ac1ffb61b9e05c54761062f0ed39ac3d55a7a
Anna <annamartinez0212@gmail.com>
2019-10-22 11:14:42 +0800

    corrected syntax

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Configure application
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.form.get("button")
+    if request.form.get("button"):
         return render_template("test.html")
     return render_template("index.html")
